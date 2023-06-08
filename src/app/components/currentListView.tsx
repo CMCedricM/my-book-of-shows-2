0d2b69b63f7a7bd6058f347d45d3b26f4af84e03
Cedric Men <cedricm13@outlook.com>
2023-06-08 02:32:37 -0700

    fixed formatting

--- src/app/components/currentListView.tsx
+++ src/app/components/currentListView.tsx
@@ -11,7 +11,7 @@ const CurrentListView = ({ listViewController }: CurrentListViewProps) => {
     <div className="flex max-h-full w-full max-w-full resize flex-col rounded-md bg-gradient-to-t from-deep-green/30 bg-blur-green backdrop-blur-md drop-shadow-md">
       <div className="px-2 py-4">
         <div className=" flex w-full flex-col items-center rounded-md bg-white p-4">
-          <h2 className="text-[24px] font-semibold text-black">
+          <h2 className="text-lg md:text-[24px] font-semibold text-black">
             {currentList}
           </h2>
         </div>
