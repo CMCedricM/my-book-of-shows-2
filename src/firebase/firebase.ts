395fb2a917c872a0679ca9a76c17d8654e926a5c
Cedric Men <cedricm13@outlook.com>
2023-06-10 15:58:32 -0700

    added test store

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4iSbZu2Tg9Gw-uaZFca6HS_HgDc6QyJ0",
+  authDomain: "my-book-of-shows-2.firebaseapp.com",
+  projectId: "my-book-of-shows-2",
+  storageBucket: "my-book-of-shows-2.appspot.com",
+  messagingSenderId: "862728329910",
+  appId: "1:862728329910:web:4ce580a30651c041176417",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
